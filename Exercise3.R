e9a79e31d3719f2f78878f88830da5e1d82ab20b
richard.kentos <richard.kentos@the-software.sk>
2023-03-06 14:03:18 +0100

    let's pray

---
+++ Exercise3.R
@@ -0,0 +1,23 @@
+library(ggplot2)
+
+#load the dataset
+data <- read.csv("data.csv")
+
+#fit a regression model
+model <- lm(Time ~ Pairs, data=data)
+
+
+# Scatterplot with scale_x_log10() and scale_y_log10()
+log_plot <- ggplot(data = data, aes(x=Pairs, y=Time)) +
+  geom_point() + scale_y_log10()
+print(log_plot)
+
+#sqrt(time)
+sqrt_plot <- ggplot(data = data, aes(x=Pairs, y=Time)) +
+  geom_point() + scale_y_sqrt()
+print(sqrt_plot)
+
+#1/time
+inverse_plot <- ggplot(data = data, aes(x=Pairs, y=1/Time)) +
+  geom_point()
+print(inverse_plot)

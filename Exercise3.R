113ed6d9a49f2d60b2bac19d5896b463326a5040
adam-rosenorn <adam.rosenoern@outlook.dk>
2023-03-09 15:04:19 +0100

    Update Exercise3.R

--- Exercise3.R
+++ Exercise3.R
@@ -3,21 +3,41 @@ library(ggplot2)
 #load the dataset
 data <- read.csv("data.csv")
 
-#fit a regression model
-model <- lm(Time ~ Pairs, data=data)
+# Create the models with different transformations
+full_model_log <- lm(log(Time) ~ Pairs * Size * Status, data = data)
 
+full_model_sqrt <- lm(sqrt(Time) ~ Pairs * Size * Status, data = data)
 
-# Scatterplot with scale_x_log10() and scale_y_log10()
-log_plot <- ggplot(data = data, aes(x=Pairs, y=Time)) +
-  geom_point() + scale_y_log10()
-print(log_plot)
+full_model_div <- lm(1/Time ~ Pairs * Size * Status, data = data)
 
-#sqrt(time)
-sqrt_plot <- ggplot(data = data, aes(x=Pairs, y=Time)) +
-  geom_point() + scale_y_sqrt()
-print(sqrt_plot)
+# Create a residual plot for log
+residual_plot_log <- ggplot(data, aes(x = Pairs, y = resid(full_model_log))) +
+  geom_point() +
+  facet_wrap(~ Size + Status, nrow = 2) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Number of Pairs") +
+  ylab("Residuals") +
+  ggtitle("Residual Plot for log")
+full_model <- lm(1/(Time) ~ Pairs * Size * Status, data = data)
 
-#1/time g
-inverse_plot <- ggplot(data = data, aes(x=Pairs, y=1/Time)) +
-  geom_point()
-print(inverse_plot)
+# Create a residual plot for sqrt
+residual_plot_sqrt <- ggplot(data, aes(x = Pairs, y = resid(full_model_sqrt))) +
+  geom_point() +
+  facet_wrap(~ Size + Status, nrow = 2) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Number of Pairs") +
+  ylab("Residuals") +
+  ggtitle("Residual Plot for sqrt")
+
+# Create a residual plot
+residual_plot_div <- ggplot(data, aes(x = Pairs, y = resid(full_model_div))) +
+  geom_point() +
+  facet_wrap(~ Size + Status, nrow = 2) +
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  xlab("Number of Pairs") +
+  ylab("Residuals") +
+  ggtitle("Residual Plot for 1/Time")
+
+print(residual_plot_log)
+print(residual_plot_sqrt)
+print(residual_plot_div)
